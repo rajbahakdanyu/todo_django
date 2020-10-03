4418228a1536f5bb406978b6d110f94b748b99a2
Danyu Rajbahak <35065226+rajbahakdanyu@users.noreply.github.com>
2020-10-03 12:49:55 +0545

    First commit

---
+++ tasks/views.py
@@ -0,0 +1,20 @@
+from django.shortcuts import render, redirect
+
+from .models import *
+from .forms import TaskForm
+
+def index(request):
+    tasks = Task.objects.all()
+
+    form = TaskForm()
+
+    if request.method =="POST":
+        form = TaskForm(request.POST)
+
+        if form.is_valid():
+            form.save()
+        return 
+
+    context = {'tasks':tasks, 'form':form}
+
+    return render(request, 'content.html', context)
